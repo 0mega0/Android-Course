68bb88ffc016b0307034ee7039a5ea30349f25a1
0mega0 <1592387105@qq.com>
2020-11-15 17:37:52 +0800

    finished

--- exp_2/app/src/main/java/com/example/vocabularybook/Activity/HelpActivity.java
+++ exp_2/app/src/main/java/com/example/vocabularybook/Activity/HelpActivity.java
@@ -1,51 +1,20 @@
-package com.example.vocabularybook.Activity;
+package com.example.vocabularybook.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vocabularybook.R;
 
+
 public class HelpActivity extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.option_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id=item.getItemId();
-        Intent intent=new Intent();
-        switch (id){
-            case R.id.mi_dictionary:
-                intent.setClass(this,MainActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            case R.id.mi_wordnote:
-                intent.setClass(this,HelpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            case R.id.mi_help:
-                intent.setClass(this,HelpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
+
 }
--- exp_2/app/src/main/java/com/example/vocabularybook/Activity/MainActivity.java
+++ exp_2/app/src/main/java/com/example/vocabularybook/Activity/MainActivity.java
@@ -1,53 +1,32 @@
-package com.example.vocabularybook.Activity;
+package com.example.vocabularybook.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vocabularybook.R;
+import com.example.vocabularybook.fragment.ComposeFragment;
+import com.example.vocabularybook.fragment.NoteListFragment;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.option_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id=item.getItemId();
-        Intent intent=new Intent();
-        switch (id){
-            case R.id.mi_dictionary:
-                intent.setClass(this,MainActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            case R.id.mi_wordnote:
-                intent.setClass(this,HelpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            case R.id.mi_help:
-                intent.setClass(this,HelpActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int h = getWindowManager().getDefaultDisplay().getHeight();
+        int w = getWindowManager().getDefaultDisplay().getWidth();
+        if (h < w) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.first_content, new NoteListFragment())
+                    .replace(R.id.second_content, new ComposeFragment())
+                    .commit();
+        } else {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.first_content, new NoteListFragment())
+                    .commit();
+        }
     }
-
 }
